cde0f9e44a29b59a60224b6ea1020dc9cffad419
Tom <thomas.ulman@gmail.com>
2018-04-05 21:35:00 +1000

    initiated node, added express and body parser, rewrote to work with post request instead of file input

--- parse.js
+++ parse.js
@@ -1,42 +1,44 @@
-// include file system module
-var fs = require('fs');
+var express = require('express')
+  , bodyParser = require('body-parser');
 
-// read file sample.json file
-fs.readFile('sample_request.json',
-    // callback function that is called when reading file is done
-    function(err, data) {
-        // json data
-        var jsonData = data;
+var app = express();
 
-        // parse json
-        var jsonParsed = JSON.parse(jsonData);
+app.use(bodyParser.json());
 
-        //From the list of shows in the request payload, 
-        //return the ones with DRM enabled (drm: true) 
-        //and at least one episode (episodeCount > 0).
-        var result = jsonParsed.payload.filter(function(el) {
+app.post('/', function(request, response){
+  console.log(request.body);      // your JSON
+
+  var jsonData = request.body;
+    
+  //From the list of shows in the request payload, 
+  //return the ones with DRM enabled (drm: true) 
+  //and at least one episode (episodeCount > 0).  
+  var result = jsonData.payload.filter(function(el) {
             return el.drm == true &&
                 el.episodeCount >= 1;
         });
 
-        //The returned JSON should have a response key 
+  //The returned JSON should have a response key 
         //with an array of shows. Each element should have 
         //the following fields from the request:
         //image - corresponding to image/showImage from the request payload
         //slug
         //title
-        response = [];
+        responseData = [];
         for (i = 0; i < result.length; i++) {
 
-            response.push({
+            responseData.push({
                 image: result[i].image.showImage,
                 slug: result[i].slug,
                 title: result[i].title
             });
         }
 
-        var response = JSON.stringify({ response });
+    var responseData = JSON.stringify({ responseData });
 
-        console.log(response);
 
-    });
+
+   response.send(responseData);    // echo the result back
+});
+
+app.listen(3000);  // your JSON
