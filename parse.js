28795c710e938be600befc614901afeb532d1618
Tom <thomas.ulman@gmail.com>
2018-04-06 10:38:32 +1000

    testing alternate port, added node v back in

--- parse.js
+++ parse.js
@@ -1,6 +1,9 @@
 var express = require('express'),
 bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
+});
 
 
 var app = express();
